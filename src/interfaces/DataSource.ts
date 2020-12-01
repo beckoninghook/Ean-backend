14a0f5b9504aba9a3ac7ef96b4f2049da825d8a8
Jelle <zuiderveldjelle@gmail.com>
2020-12-01 15:30:34 +0100

    i can push?

--- src/interfaces/DataSource.ts
+++ src/interfaces/DataSource.ts
@@ -4,5 +4,4 @@ import {FoodProduct} from "../models/FoodProduct";
 export interface DataSource {
     searchBarcode: (barcode: number) => FoodProduct[];
 
-    getFoodProducts():
 }
--- src/models/FoodProduct.ts
+++ src/models/FoodProduct.ts
@@ -4,15 +4,13 @@ export class FoodProduct {
     calories: Number
     carbohydrates: Number
     label: String
-    barcode: Number
 
 
-    constructor(id: Number, calories: Number, carbohydrates: Number, label: String, barcode: Number) {
+    constructor(id: Number, calories: Number, carbohydrates: Number, label: String) {
         this.id = id;
         this.calories = calories;
         this.carbohydrates = carbohydrates;
         this.label = label;
-        this.barcode = barcode;
     }
 
 
