c0a29e7ec062dbbb5f13a508a28801a6bfd73c48
Jelle <zuiderveldjelle@gmail.com>
2020-12-01 14:57:25 +0100

    model

--- src/interfaces/DataSource.ts
+++ src/interfaces/DataSource.ts
@@ -1,5 +1,8 @@
 import {FoodProduct} from "../models/FoodProduct";
 
+
 export interface DataSource {
     searchBarcode: (barcode: number) => FoodProduct[];
+
+    getFoodProducts():
 }
--- src/models/FoodProduct.ts
+++ src/models/FoodProduct.ts
@@ -1,3 +1,20 @@
 export class FoodProduct {
 
+    id: Number
+    calories: Number
+    carbohydrates: Number
+    label: String
+    barcode: Number
+
+
+    constructor(id: Number, calories: Number, carbohydrates: Number, label: String, barcode: Number) {
+        this.id = id;
+        this.calories = calories;
+        this.carbohydrates = carbohydrates;
+        this.label = label;
+        this.barcode = barcode;
+    }
+
+
+
 }
