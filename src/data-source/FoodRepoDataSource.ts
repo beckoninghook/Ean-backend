3e1fe8ff1fee2a9da30b4a462b71009346e2f922
Jelle <zuiderveldjelle@gmail.com>
2020-12-17 10:49:27 +0100

    corrected the api key

--- src/data-source/FoodRepoDataSource.ts
+++ src/data-source/FoodRepoDataSource.ts
@@ -2,18 +2,27 @@ import {DataSource} from "../interfaces/DataSource";
 import {FoodProduct} from "../models/FoodProduct";
 // @ts-ignore
 import axios from 'axios';
+import {request} from "express";
+
 
 export class FoodRepoDataSource implements DataSource{
-
+    
     dataSourceIndicator: string = "FoodRepo"
-    url: String = 'https://www.foodrepo.org/api/v3/products'
-    apiKey: 'a72bc81fa19d14402677050a0133b1bc'
+    url: String = 'https://www.foodrepo.org/api/v3/products/'
+    apiKey: '052da85a97d58565de47584f7e516dcc'
+
+    options = {
+        url: 'https://www.foodrepo.org/api/v3/products/',
+        headers: {'Authorization': 'Token token=052da85a97d58565de47584f7e516dcc'}
+    };
+
 
     constructor() {
     }
 
+
     async searchBarcode(barcode: number): Promise<FoodProduct[]> {
-        const data = await axios.get(this.url + barcode.toString())
+        const data = await axios.get(this.options +'2030')
         if (data.data.status == 0) {
             return [];
         }
