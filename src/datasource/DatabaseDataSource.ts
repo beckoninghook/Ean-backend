050b395de6ecffcffe17fc7054f57435e7c73d31
Jelle <zuiderveldjelle@gmail.com>
2021-01-02 16:32:43 +0100

    Get product from database

--- src/datasource/DatabaseDataSource.ts
+++ src/datasource/DatabaseDataSource.ts
@@ -1,12 +1,17 @@
 import {DataSource} from "../interfaces/DataSource";
 import {FoodProduct} from "../models/FoodProduct";
+import {SequelizeFoodProduct} from "../database/db-models/SequelizeFoodProduct";
 
 // TODO: When searchBarcode is called, use sequelize to search for product where barcode matches. If not found, return empty array.
 export class DatabaseDataSource implements DataSource {
     dataSourceIndicator = "EAN-Backend Database"
 
+
+
     async searchBarcode(barcode: number): Promise<FoodProduct[]> {
-        return []
+        const data = await SequelizeFoodProduct.findOne({where:{eanBarcode: barcode.toString()}})
+
+        return [data]
     }
 
     convertData(data: any): Promise<FoodProduct[]> {
