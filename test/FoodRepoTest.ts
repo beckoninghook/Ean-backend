525b50638e636f57f504ba782f4ab23a38a30944
Jelle <zuiderveldjelle@gmail.com>
2021-01-15 14:58:26 +0100

    progress

--- test/FoodRepoTest.ts
+++ test/FoodRepoTest.ts
@@ -1,20 +1,21 @@
 import {FoodProduct} from "../src/models/FoodProduct";
 import {FoodRepoDataSource} from "../src/datasource/FoodRepoDataSource";
 
+var expect = require('chai').expect;
 const assert = require('assert');
-const { foodProduct } = require('../src/models/FoodProduct')
-
-function asyncMethod(){
-    return new Promise<void>((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, 1000)
-    })
-}
 
 describe('searchBarcode', () => {
-    it('should return a foodproduct ',  async function (){
+    it('should return a foodproduct ',function (done){
+        var food = null;
+        var result = new FoodRepoDataSource().searchBarcode(0);
 
+        result.then(function (data){
+            expect(data).to.equal(food);
+            done();
+        },function (error){
+            assert.fail(error)
+            done();
+        });
     });
 });
 
