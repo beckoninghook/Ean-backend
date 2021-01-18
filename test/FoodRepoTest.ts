d47ff73287cdcf1b55996b5e7237debaa85704c7
Jelle <zuiderveldjelle@gmail.com>
2021-01-18 12:38:16 +0100

    test for foodrepo

--- test/FoodRepoTest.ts
+++ test/FoodRepoTest.ts
@@ -1,22 +1,58 @@
 import {FoodProduct} from "../src/models/FoodProduct";
 import {FoodRepoDataSource} from "../src/datasource/FoodRepoDataSource";
+import {it} from "mocha";
 
-var expect = require('chai').expect;
+const request = require('request')
 const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const urlSearch = 'https://www.foodrepo.org/api/v3/products?barcodes='
+const barcode = 7613404377888;
 
-describe('searchBarcode', () => {
-    it('should return a foodproduct ',function (done){
-        var food = null;
-        var result = new FoodRepoDataSource().searchBarcode(0);
 
-        result.then(function (data){
-            expect(data).to.equal(food);
+
+describe('FoodRepo Tests', function (){
+    it('should give acces denied without api key', function (done){
+        request.get({
+            url: urlSearch + barcode.toString()
+        },
+            function (error,respone,body){
+                let _body = {};
+                try{
+                _body = JSON.parse(body);
+            }
+            catch (e){
+                _body = {};
+            }
+
+            expect(respone.statusCode).to.equal(401);
+
             done();
-        },function (error){
-            assert.fail(error)
-            done();
-        });
+            });
     });
-});
+
+    it('should give a foodproduct',function (done){
+        request.get({
+                url: urlSearch + barcode.toString(),headers: {
+                    'Authorization': 'Token token=d558bf263e21e34ad76c95c0f006d0de'
+                }
+            },
+            function (error,respone,body){
+                let _body = {};
+                try{
+                    _body = JSON.parse(body);
+                }
+                catch (e){
+                    _body = {};
+                }
+
+                expect(respone.statusCode).to.equal(200);
+                assert.notStrictEqual(_body,null, "should not be null");
+                console.log(_body)
+                done();
+            });
+    });
+
+})
 
 
